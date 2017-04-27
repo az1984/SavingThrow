2c35589add2cea1ff6d1f12ed76631522bbd25ca
az1984 <andrew.e.zimmer@gmail.com>
2017-04-27 12:27:47 -0500

    Update InstallSavingThrow.sh

--- InstallSavingThrow.sh
+++ InstallSavingThrow.sh
@@ -1,16 +1,47 @@
 #!/bin/bash
+#PURPOSE:   Install a modified version of the "SavingThrow" script for malware detection.
+#           The original project is at "https://github.com/sheagcraig/SavingThrow"
+#           Last edited by Andrew Zimmer on 04/27/2017
+
+##
+#Variables
+##
+#Date for date-stamping the logs
 date=`date +"%Y_%m_%d"`
 
+##
+#Directories
+##
 #Create the directories to store definition files
 mkdir -p /Library/HuskerFiles/SavingThrow/Support/
 #Create the directories to store definition files
 mkdir -p /Library/HuskerFiles/SavingThrow/Logs/
 
-#Download the current SavingThrow script(s) from GitHub.
-wget -c https://raw.githubusercontent.com/az1984/SavingThrow/
+##
+##Preparation
+##
+#Change to /tmp directory
+cd /tmp
 
-#Copy all shell scripts to 
+#Download the current SavingThrow script from GitHub.
+https://raw.githubusercontent.com/az1984/SavingThrow/master/ATH_SavingThrow.py
+#Download the wrapper script used for one-off scans.
+https://raw.githubusercontent.com/az1984/SavingThrow/master/Run_SavingThrow.sh
+
+#Copy all scripts to installation directory
 cp -rv ./*.sh /Library/HuskerFiles/SavingThrow/
 cp -rv ./*.py /Library/HuskerFiles/SavingThrow/
+
+#Change to installation directory
 cd /Library/HuskerFiles/SavingThrow/
-#python ./ATH_SavingThrow.py -v >> /Library/HuskerFiles/SavingThrow/scan_$date.log
+
+#Make the scripts executable.
+chmod a+rx *.sh
+chmod a+rx *.py
+
+##
+##Perform first scan
+##
+The script is run with "verbose" (-v) and "remove" (-r) options
+#This deletes any malware found and logs the activity to the "Logs" folder.
+python ATH_SavingThrow.py -v -r >> /Library/HuskerFiles/SavingThrow/Logs/scan_$date.log
